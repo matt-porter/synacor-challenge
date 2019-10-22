3ebebb42451166a2161ea1118c9143d49669fe26
Matt Porter <matt.porter@cityfibre.com>
2019-10-22 15:23:48 +0100

    Add route visualisation

--- player.py
+++ player.py
@@ -8,8 +8,19 @@ import sys
 
 import pexpect
 
+def route_to_dot(route):
+    uniq = set()
+    with open('route.dot', 'w') as f:
+        f.write('digraph g {\n')
+        for ((choice1, loc_id1), (choice2, loc_id2)) in zip(route, route[1:]):
+            if (loc_id1, loc_id2, choice2) not in uniq:
+                f.write('{} -> {} [label={}];\n'.format(loc_id1, loc_id2, choice2))
+                uniq.add((loc_id1, loc_id2, choice2))
+        f.write('}\n')
+    pexpect.spawn('dot -x -Tpng route.dot -o route.png')
+
 def find_location(text):
-    return hashlib.sha256(''.join(text.split('\r\n'))[:2].encode()).hexdigest()
+    return 'L' + hashlib.sha256(''.join(text.split('\r\n'))[:2].encode()).hexdigest()
 
 def find_list(text, heading):
     """Find the list items under heading in text"""
@@ -29,7 +40,7 @@ def find_list(text, heading):
     return options
 
 def run():
-    history = defaultdict(list)
+    this_route = []
     child = pexpect.spawn('target/release/synacor')
     last_options = []
     for move in range(1000):
@@ -40,9 +51,16 @@ def run():
         except pexpect.EOF:
             print(child.before.decode())
             print('exiting on EOF')
+            this_route[-1] = ((this_route[-1][0], 'EOF'))
+            print(route_to_dot(this_route))
             sys.exit(1)
         output = child.after.decode()
         location_id = find_location(output)
+        # replace last element to incl. location id
+        if this_route:
+            this_route[-1] = ((this_route[-1][0], location_id))
+        else:
+            this_route.append(('START', location_id))
         items = find_list(output, 'Things of interest here:')
         options = find_list(output, 'There (is|are) \d+ exits?:')
         if not options:
@@ -52,20 +70,11 @@ def run():
             print('Location: {} Take: {}'.format(location_id, i))
             child.sendline('take ' + i)
             continue
-        unvisited = [o for o in options if o not in history[location_id]]
-        if unvisited:
-            options = unvisited
-        try:
-            choice = random.choice(options)
-        except:
-            print(output)
-            print(options, unvisited)
-            print('Exiting on exception in random.choice')
-            sys.exit(1)
-        else:
-            history[location_id].append(choice)
-            child.sendline(choice)
-            print('{}: Location: {} Options: {} Unvisited: {} Chose: {}'.format(move, location_id, options, unvisited, choice))
+        choice = random.choice(options)
+        this_route.append((choice,None))
+        child.sendline(choice)
+        print('{}: Location: {} Options: {} Chose: {}'.format(move, location_id, options, choice))
+
 
 
 if __name__ == '__main__':
